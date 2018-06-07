0009581d2d7920717f3b48cf241f42e7bd660128
Ronald Mariah <ronaldmariah@gmail.com>
2018-06-07 23:41:37 +0200

    Run migrations and seeding on startup

---
+++ CakeTickBoard/Migrations/DatabaseInitializerHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace CakeTickBoard.Migrations
+{
+    public static class DatabaseInitializerHelper
+    {
+        public static void AutoApplyMigrations(DbMigrator dbMigrator)
+        {
+            ApplyForwardPendingMigrations(dbMigrator);
+            ApplyRollBackMigrations(dbMigrator);
+        }
+
+        private static void ApplyForwardPendingMigrations(DbMigrator dbMigrator)
+        {
+            dbMigrator.Update();
+        }
+
+        private static void ApplyRollBackMigrations(DbMigrator dbMigrator)
+        {
+            var latestMigrationAppliedToDatabase = dbMigrator.GetDatabaseMigrations().OrderBy(x => x).Last();
+            var latestMigrationDefinedInCode = dbMigrator.GetLocalMigrations().OrderBy(x => x).Last();
+
+            if (latestMigrationAppliedToDatabase.Equals(latestMigrationDefinedInCode))
+                return;
+
+            dbMigrator.Configuration.AutomaticMigrationDataLossAllowed = true;
+            dbMigrator.Update(latestMigrationDefinedInCode);
+        }
+    }
+}
