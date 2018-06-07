29317064faf9a606f91fd12e9828e8d8bb860e17
Ronald Mariah <ronaldmariah@gmail.com>
2018-06-07 21:53:21 +0200

    Initial commit

---
+++ CakeTickBoard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CakeTickBoard.MvcApplication" Language="C#" %>
---
+++ CakeTickBoard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CakeTickBoard.Startup))]
+namespace CakeTickBoard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
