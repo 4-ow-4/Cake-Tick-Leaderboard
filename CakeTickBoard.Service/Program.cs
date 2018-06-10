9f6382fd8f3a91211eb2472b5e5dcf6cb13cd6e3
Ronald Mariah <ronaldmariah@gmail.com>
2018-06-10 20:20:00 +0200

    Projects in the new architecture. Application Logic Windows Service which will use Hangfire Web Feed Repository for EF Core DI Container for resolving dependencies

---
+++ CakeTickBoard.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CakeTickBoard.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
